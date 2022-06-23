dbfafaec2193a6377c022375500bee81bce55420
bradley <bradleybkempton@gmail.com>
2022-06-23 07:11:45 -0700

    added ship factory to ship.js

--- ship.js
+++ ship.js
@@ -1,4 +1,17 @@
 function ship(a, b) {
+    const Ship = (length) => {
+        const length = length;
+        const hit = () => console.log("hit"); //function that takes a number and then marks that position as ‘hit'
+        const isSunk= () => console.log("sunk"); //unction that calculates it based on their length and whether all of their positions are ‘hit’.
+        //return {length};
+      }
+
+
+
+
+
+
+
     return a + b;
   }
   module.exports = ship;
